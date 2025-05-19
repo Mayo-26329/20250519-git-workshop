83824a2d59975aebe3cce77def29caab27841173
Yamaguchi Mayo <184481668+Mayo-26329@users.noreply.github.com>
2025-05-19 14:25:38 +0900

    ピースの色を変更
    blueをskyblueに変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'indigo', 'orange', 'blue', 'yellow', 'coral', 'lime', 'purple'
+    'indigo', 'orange', 'skyblue', 'yellow', 'coral', 'lime', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
