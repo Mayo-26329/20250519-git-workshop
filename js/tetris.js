68f48fbb41feae28148b8acc9f332b08c681989d
Yamaguchi Mayo <241205177@ccmailg.meijo-u.ac.jp>
2025-05-19 14:20:35 +0900

    ピースの色を変更
    indigoをskyblueに変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'indigo', 'orange', 'blue', 'yellow', 'coral', 'lime', 'purple'
+    'skyblue', 'orange', 'blue', 'yellow', 'coral', 'lime', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
